72e627bf51a46e1358cee94025bc359f7539526f
m_sekine <m_sekeine@fujiball.co.jp>
2017-09-22 00:47:24 +0900

    try〜chach文練習

--- ECMAScript/js/free.js
+++ ECMAScript/js/free.js
@@ -1,26 +1,38 @@
-const obj = {
-  statusCode: 301,
-  headers: {
-    Location: "test.html",
-  },
-  body: "token"
+//const obj = {
+//  statusCode: 301,
+//  headers: {
+//    Location: "test.html",
+//  },
+//  body: "token"
+//}
+//console.log(obj);
+//
+//const lamdba = { statusCode: 201 };
+//
+//const test = Object.assign({}, lamdba);
+////console.log(lamdba);
+////console.log(test);
+//
+//
+//const handler = (url) => {
+//  let response = { statusCode: 301};
+//  let header = { header: {location: url}};
+//  return url ? Object.assign({}, response, { header: {location: url}}) : "hoge";
+//};
+
+//const result = handler("test!");
+//console.log(result);
+
+try {
+  let test = "hoge";
+  if (!test) {
+    console.log(test);
+  } else {
+    throw new Error("errorだよ");
+  }
+
+} catch (e) {
+  console.log("hoge");
+  console.log(e.message);
 }
-console.log(obj);
-
-const lamdba = { statusCode: 201 };
-
-const test = Object.assign({}, lamdba);
-//console.log(lamdba);
-//console.log(test);
-
-
-const handler = (url) => {
-  let response = { statusCode: 301};
-  let header = { header: {location: url}};
-  return url ? Object.assign({}, response, { header: {location: url}}) : "hoge";
-};
-
-const result = handler("test!");
-console.log(result);
-
 
